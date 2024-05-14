eb320fba42ea463c00dc3e1878f6c48ff0964f83
Nichlas <141312928+NichLD@users.noreply.github.com>
2024-05-14 09:58:28 +0200

    fixed small error

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // let calculator = require('./utils/calculator');
 let { add, subtract } = require('./modules/calculator');
 
-console.log(calculator.add(1, 6));
-console.log(calculator.subtract(1, 6));
+console.log(add(1, 6));
+console.log(subtract(1, 6));
 // console.log(calculator.multiply(1, 6));
 // console.log(calculator.divide(1, 6));
+// console.log(calculator.add(1, 6));
+// console.log(calculator.subtract(1, 6));
