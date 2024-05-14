c13bd9006fa0019f6e5a183cfdca9d0699978344
Nichlas <141312928+NichLD@users.noreply.github.com>
2024-05-14 09:46:19 +0200

    mer

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+// let calculator = require('./utils/calculator');
 let { add, subtract } = require('./utils/calculator');
 
-console.log(add(1, 6));
-console.log(subtract(1, 6));
+console.log(calculator.add(1, 6));
+console.log(calculator.subtract(1, 6));
+// console.log(calculator.multiply(1, 6));
+// console.log(calculator.divide(1, 6));
