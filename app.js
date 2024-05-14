94660db90b5cd32c87da2a4e529a1f550fb919aa
Nichlas <141312928+NichLD@users.noreply.github.com>
2024-05-14 09:43:16 +0200

    Added shit for you motthafucka

---
+++ app.js
@@ -0,0 +1,4 @@
+let { add, subtract } = require('./utils/calculator');
+
+console.log(add(1, 6));
+console.log(subtract(1, 6));
---
+++ utils/calculator.js
@@ -0,0 +1,23 @@
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function add(a, b) {
+    return a + b;
+    }
+
+function subtract(a, b) {
+    return a - b;
+}
+
+module.exports = {
+  divide,
+  multiply,
+  add,
+  subtract
+};
+
